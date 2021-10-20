e2883d04d887ec44c166af4527d34157c57a6531
TomasJakovlevas <tomasjakovlevas@gmail.com>
2021-10-20 16:53:44 +0300

    FEATURE: dropdown menu

--- 1_frontend/src/components/Header/Header.jsx
+++ 1_frontend/src/components/Header/Header.jsx
@@ -1,45 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 // Styling
-import { StyledHeader, StyledNavigation } from './Header.styled';
+import {
+  StyledHeader,
+  StyledNavigation,
+  StyledMobileNavigation,
+  MobileMenuButton,
+} from './Header.styled';
 
 // Fontawesom
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneAlt, faBars } from '@fortawesome/free-solid-svg-icons';
+
+// Hamburger
+import { Sling as Hamburger } from 'hamburger-react';
+
 const Header = () => {
+  // Hooks
+  // -- state
+  // --- local
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  // Custom functions
+  const showMenu = () => setMenuOpen((prev) => !prev);
+
   return (
-    <StyledHeader>
-      <div>LOGO</div>
-      <StyledNavigation>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/about'>About us</Link>
-          </li>
-          <li>
-            <Link to='/services'>Services</Link>
-          </li>
-          <li>
-            <Link to='/blog'>Blog</Link>
-          </li>
-          <li>
-            <Link to='/contact'>Contact</Link>
-          </li>
-        </ul>
-        <div className='quickCall_div'>
-          <div className='quiclCall_div__phone'>
-            <FontAwesomeIcon icon={faPhoneAlt} />
+    <>
+      <StyledHeader>
+        <div>LOGO</div>
+        <StyledNavigation>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About us</Link>
+            </li>
+            <li>
+              <Link to='/services'>Services</Link>
+            </li>
+            <li>
+              <Link to='/blog'>Blog</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+          </ul>
+          <div className='quickCall_div'>
+            <div className='quiclCall_div__phone'>
+              <FontAwesomeIcon icon={faPhoneAlt} />
+            </div>
+            <div>
+              <span>Road Assistance</span>
+              <h4>1800 265 24 52</h4>
+            </div>
           </div>
-          <div>
-            <span>Road Assistance</span>
-            <h4>1800 265 24 52</h4>
+        </StyledNavigation>
+        <MobileMenuButton onClick={(e) => showMenu(e)}>
+          <Hamburger />
+        </MobileMenuButton>
+      </StyledHeader>
+      {menuOpen && (
+        <StyledMobileNavigation>
+          <ul onClick={() => setMenuOpen(false)}>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About us</Link>
+            </li>
+            <li>
+              <Link to='/services'>Services</Link>
+            </li>
+            <li>
+              <Link to='/blog'>Blog</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+          </ul>
+          <div className='quickCall_div'>
+            <div>
+              <span>Road Assistance</span>
+              <h4>1800 265 24 52</h4>
+            </div>
           </div>
-        </div>
-      </StyledNavigation>
-    </StyledHeader>
+        </StyledMobileNavigation>
+      )}
+    </>
   );
 };
 
--- 1_frontend/src/components/Header/Header.styled.js
+++ 1_frontend/src/components/Header/Header.styled.js
@@ -1,13 +1,20 @@
 import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
-  width: 100%;
+  width: 90%;
   max-width: 1200px;
   margin: 0 auto;
   height: 90px;
   left: 0px;
   top: 0px;
 
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const StyledNavigation = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -21,6 +28,11 @@ export const StyledHeader = styled.header`
     display: inline;
     padding: 5px 8px;
     margin: 0 10px;
+  }
+
+  ul li a {
+    text-decoration: none;
+    color: black;
   }
 
   .quickCall_div {
@@ -53,10 +65,59 @@ export const StyledHeader = styled.header`
       font-weight: 600;
     }
   }
+
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
-export const StyledNavigation = styled.nav`
+export const StyledMobileNavigation = styled.nav`
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  align-items: flex-end;
+  background-color: #f4f4f4;
+  width: 90%;
+  margin: 0 auto;
+
+  ul {
+    list-style-type: none;
+  }
+
+  ul li {
+    text-align: right;
+    padding: 5px;
+    border-bottom: 1px solid black;
+    margin: 5px 0;
+  }
+
+  ul li a {
+    text-decoration: none;
+    color: black;
+  }
+
+  .quickCall_div {
+    margin-top: 10px;
+    padding: 5px;
+  }
+
+  @media only screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const MobileMenuButton = styled.div`
+  font-size: 30px;
+  width: 50px;
+  height: 50px;
+  background-color: #61c3a1;
+  color: white;
+  cursor: pointer;
+
+  display: flex;
+  justify-content: center;
   align-items: center;
+
+  @media only screen and (min-width: 768px) {
+    display: none;
+  }
 `;
